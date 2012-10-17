a50e95c6a0659a366b39436196cfafca287f9368
fkereki <fkereki@gmail.com>
2012-10-17 20:03:03 -0200

    Cambio trivial

--- src/com/kereki/gwtsampleproject/client/GetWorldTimeService.java
+++ src/com/kereki/gwtsampleproject/client/GetWorldTimeService.java
@@ -4,9 +4,9 @@ import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
 /**
- * The client side stub for the RPC service.
+ * The client side stub for the RPC service
  */
 @RemoteServiceRelativePath("getworldtime")
 public interface GetWorldTimeService extends RemoteService {
-    String getWorldTime(String zone);
+  String getWorldTime(String zone);
 }
